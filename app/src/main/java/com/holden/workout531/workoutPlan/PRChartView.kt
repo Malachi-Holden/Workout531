50f933e59b43811919ba4aa8169017b3ecc74335
Malachi <malachi.coqui@gmail.com>
2023-09-02 15:01:21 -0400

    prettify pr chart

--- app/src/main/java/com/holden/workout531/workoutPlan/PRChartView.kt
+++ app/src/main/java/com/holden/workout531/workoutPlan/PRChartView.kt
@@ -1,14 +1,24 @@
 package com.holden.workout531.workoutPlan
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.holden.workout531.workout.Workout
@@ -16,9 +26,19 @@ import com.holden.workout531.workout.pr
 
 @Composable
 fun PRChartView(chart: List<Pair<String, List<Pair<String, List<Pair<String, Int>>>>>>){
-    LazyRow {
-        items(chart){period ->
+    LazyColumn {
+        items(chart.size - 1){
+            val period = chart[it]
             PeriodView(period.first, period.second)
+            if (chart.size > 1){
+                Divider(modifier = Modifier.padding(vertical = 5.dp))
+            }
+        }
+        if (chart.size > 1){
+            item {
+                val period = chart.last()
+                PeriodView(period.first, period.second)
+            }
         }
     }
 }
@@ -27,13 +47,16 @@ fun PRChartView(chart: List<Pair<String, List<Pair<String, List<Pair<String, Int
 fun PeriodView(periodName: String, period: List<Pair<String, List<Pair<String, Int>>>>){
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.padding(horizontal = 10.dp)
+        modifier = Modifier
+            .padding(horizontal = 10.dp)
     ) {
-        Row {
+        Row(modifier = Modifier.horizontalScroll(rememberScrollState())) {
             for (day in period){
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.padding(horizontal = 10.dp)
+                    modifier = Modifier
+                        .padding(horizontal = 10.dp)
+                        .border(2.dp, MaterialTheme.colorScheme.onBackground.copy(alpha = .5f))
                 ) {
                     Row {
                         for (workout in day.second){
